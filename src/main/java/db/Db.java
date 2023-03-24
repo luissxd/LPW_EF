3c0cff6367b557b91a6a99f52f8a3d59c8f8abed
luissxd <luiss.a@hotmail.com>
2023-03-23 22:55:45 -0500

    Se actualiza archivos

--- src/main/java/db/Db.java
+++ src/main/java/db/Db.java
@@ -7,10 +7,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Db {
-	String _IP = "localhost", _PORT = "1433", _USER = "sa", _PASSWORD = "xd", _BD = "", _SQL = "";
+	String _IP ="localhost", _PORT="1433", _USER="sa", _PASSWORD="sa.", _BD="", _SQL="";
 	Connection cn = null;
 	PreparedStatement ps = null;
-
+	
 	public Db(String bd) {
 		this._BD = bd;
 		getConnection();
@@ -36,90 +36,72 @@ public class Db {
 	public void getConnection() {
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			cn = DriverManager.getConnection(
-					String.format("jdbc:sqlserver://%s:%s;database=%s;encrypt=false", _IP, _PORT, _BD), _USER,
-					_PASSWORD); // ;trustServerCertificate=false
-			// cn = DriverManager.getConnection(
-			// String.format("jdbc:sqlserver://%s:%s;database=%s;integratedSecurity=true;encrypt=false",
-			// _IP, _PORT, _BD ), _USER, _PASSWORD ); // ;trustServerCertificate=false
-		} catch (ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
-		}
+			cn = DriverManager.getConnection( String.format("jdbc:sqlserver://%s:%s;database=%s;encrypt=false", _IP, _PORT, _BD ), _USER, _PASSWORD ); // ;trustServerCertificate=false 
+			//cn = DriverManager.getConnection( String.format("jdbc:sqlserver://%s:%s;database=%s;integratedSecurity=true;encrypt=false", _IP, _PORT, _BD ), _USER, _PASSWORD ); // ;trustServerCertificate=false 
+		} catch (ClassNotFoundException | SQLException e) { e.printStackTrace(); }	
 	}
-
+	
 	public void Sentencia(String sql) {
-		if (cn == null)
-			return;
-
+		if ( cn == null ) return;
+		
 		this._SQL = sql;
 		try {
 			ps = cn.prepareStatement(sql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
+		} catch (SQLException e) { e.printStackTrace(); }
+		
 	}
-
+	
 	public String[][] getRegistros() {
-		if (cn == null)
-			return null;
-
+		if ( cn == null ) return null;
+		
 		try {
 			ResultSet rs = ps.executeQuery();
-			if (rs.last()) {
+			if ( rs.last() ) {
 				int filas = rs.getRow();
 				int columnas = rs.getMetaData().getColumnCount();
-
+			
 				String[][] mRegistros = new String[filas][columnas];
 				rs.beforeFirst();
-				for (int fila = 0; rs.next(); fila++)
-					for (int columna = 0; columna < columnas; columna++)
-						mRegistros[fila][columna] = rs.getString(columna + 1).trim();
+				for (int fila = 0; rs.next(); fila++ )
+					for ( int columna=0; columna < columnas; columna++ )
+						mRegistros[fila][columna] = rs.getString( columna + 1 ).trim();
 
 				return mRegistros;
 			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
+		} catch (SQLException e) { e.printStackTrace(); }
+		
 		return null;
 	}
-
+	
+	
 	public String[] getRegistro() {
-		if (cn == null)
-			return null;
-
+		if ( cn == null ) return null;
+		
 		try {
 			ResultSet rs = ps.executeQuery();
-
-			if (rs.next()) {
+			
+			if ( rs.next() ) {
 				int columnas = rs.getMetaData().getColumnCount();
-
+				
 				String[] aRegistro = new String[columnas];
-				for (int columna = 0; columna < columnas; columna++)
-					aRegistro[columna] = rs.getString(columna + 1);
-
+				for ( int columna=0; columna < columnas; columna++ )
+					aRegistro[columna] = rs.getString( columna + 1 );
+				
 				return aRegistro;
 			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
+		} catch (SQLException e) { e.printStackTrace(); }
+		
 		return null;
 	}
 
 	public String getCampo() {
-		if (ps == null)
-			return null;
+		if ( ps == null ) return null;
 		try {
 			ResultSet rs = ps.executeQuery();
-			if (rs.next())
-				return rs.getString(1);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		return null;
+			if ( rs.next() ) return rs.getString(1);
+		} catch (SQLException e) { e.printStackTrace(); }
+		
+		return null;	
 	}
 
 }
--- src/main/java/svl/svlParticipacion.java
+++ src/main/java/svl/svlParticipacion.java
@@ -10,61 +10,52 @@ import java.io.IOException;
 
 public class svlParticipacion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
+    public svlParticipacion() {
+        super();
+    }
 
-	public svlParticipacion() {
-		super();
-	}
-
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
 		request.setCharacterEncoding("UTF-8");
 		HttpSession session = request.getSession();
 		dao.OnpeDAO daoOnpe = new dao.OnpeDAO();
-
-	
-		
+					
 		String id = request.getParameter("id");
+		String sDPD = null, sAmbito = null;
+		Object data = null, totales = null;
 		
-		String sDPD=null,sAmbito=null;
-		Object data = null,totales=null;
-
-		if (id != null) {			
-			String aID[]= id.split(",");
-			boolean bNacional=aID[0].equals("Nacional");
+		if ( id != null ) {
+			String aID[] = id.split(",");
 			
+			if ( aID.length == 1 ) sDPD = aID[0].equals("Nacional") ? "DEPARTAMENTO" : "CONTINENTE";
+			else if ( aID.length == 2 ) sDPD = aID[0].equals("Nacional") ? "PROVINCIA" : "PAÍS";
+			else if ( aID.length == 3 ) sDPD = aID[0].equals("Nacional") ? "DISTRITO" : "CIUDAD";
 			
-			if(aID.length==1) sDPD=bNacional ? "DEPARTAMENTO":"CONTINENTE";
-			else if (aID.length==2) sDPD=bNacional ? "PROVINCIA":"PAIS";
-			else if (aID.length==3) sDPD=bNacional ? "DISTRITO":"CIUDAD";
+			if ( aID.length == 1 ) data = daoOnpe.getVotos( id.equals("Nacional") ? 1 : 26, id.equals("Nacional") ? 25 : 30 );
+			else if ( aID.length == 2 ) data = daoOnpe.getVotosDepartamento( aID[1] );
+			else if ( aID.length == 3 ) data = daoOnpe.getVotosProvincia( aID[2] );
 			
-			
-			
-			if(aID.length==1) data = daoOnpe.getVotos(bNacional ? 1 : 26, bNacional ? 25 : 30);
-			else if (aID.length==2) data = daoOnpe.getVotosDepartamento( aID[1]);
-			else if (aID.length==3) data = daoOnpe.getVotosProvincia( aID[2]);
-			
-			sAmbito="Ambito: "+ aID[0];
-			if(aID.length>1) sAmbito+="<br/>" + (bNacional?"Departamento":"Continente")+": "+aID[1];
-			if(aID.length>2) sAmbito+="<br/>" + (bNacional?"Provincia":"País")+": "+aID[2];
-			if(aID.length>3) sAmbito+="<br/>" + (bNacional?"Distrito":"Ciudad")+": "+aID[3];
+			sAmbito = "Ámbito : " + aID[0];
+			if ( aID.length > 1 ) sAmbito += "<br/>" + ( aID[0].equals("Nacional") ? "Departamento" : "Continente" ) + " : " + aID[1];
+			if ( aID.length > 2 ) sAmbito += "<br/>" + ( aID[0].equals("Nacional") ? "Provincia" : "País" ) + " : " + aID[2];
+			if ( aID.length > 3 ) sAmbito += "<br/>" + ( aID[0].equals("Nacional") ? "Distrito" : "Ciudad" ) + " : " + aID[3];
 		}
-
+		
 		session.setAttribute("id", id);
-		session.setAttribute("DPD", sDPD);
-		session.setAttribute("ambito", sAmbito);
+		session.setAttribute("DPD", sDPD );
+		session.setAttribute("ambito", sAmbito );
 		session.setAttribute("data", data);
 		session.setAttribute("totales", totales);
-
+		
 		response.sendRedirect("participacion.jsp");
 	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		processRequest(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		processRequest(request, response);
 	}
 
